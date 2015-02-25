a1612da8f9023f6221aa3bd629b437d22d9a6269
skup5 <5skup5@seznam.cz>
2015-02-25 13:10:50 +0100

    jsoup sample example

--- TestApp/app/src/main/java/com/example/roman/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/roman/testapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.roman.testapp;
 
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +18,8 @@ import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
 
+    ProgressDialog mProgressDialog;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,10 +49,47 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void refresh(View v){
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(getData1("http://evropa2.cz/mp3-archiv/"));
+    private class E2 extends AsyncTask<Void, Void, Void> {
+        String title;
+        String url = "http://evropa2.cz/mp3-archiv/";
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Mp3 archiv Evropy 2");
+            mProgressDialog.setMessage("Načítání...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(url).get();
+                // Get the html document title
+                title = document.title();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set title into TextView
+            TextView txttitle = (TextView) findViewById(R.id.textView);
+            txttitle.setText(title);
+            mProgressDialog.dismiss();
+        }
+    }
+
+    public void refresh(View v){
+        /*TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(getData1("http://evropa2.cz/mp3-archiv/"));
+        */
+        new E2().execute();
     }
 
     public String getData1(String url){
