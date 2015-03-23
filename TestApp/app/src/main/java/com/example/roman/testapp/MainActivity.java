37092c89cfb35d27413fa07a384f31fe94c62368
skup5 <5skup5@seznam.cz>
2015-03-23 20:25:26 +0100

    navigation on left, drawerArrowToogle animate, test data in navigation

--- TestApp/app/src/main/java/com/example/roman/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/roman/testapp/MainActivity.java
@@ -10,13 +10,21 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +33,8 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -33,23 +43,82 @@ public class MainActivity extends ActionBarActivity {
     private AsyncPlayer ap;
     private MediaPlayer mediaPlayer;
     private Button playBt;
+    private ListView navList;
+    private TextView navHeader;
+
+    private ArrayAdapter<String> navListAdapter;
     //private String url;
+    final String[] categoryItems = new String[] {
+            "Hudební ceny Evropy 2 (2014)", "To nejlepší z Ranní show",
+            "Zpátky do minulosti", "Bombucman", "Odpolední odhalení - Ekl Zástěra",
+            "To nejlepší z Odpolední show", "To nejlepší z Víkendové ranní show",
+            "Emoce na Evropě 2", "Dance Exxtravaganza", "Novinky na DVD",
+            "Internet", "Hosté Evropy 2", "Songy Evropy 2"};
+
     /**
      * remain false till media is not completed, inside OnCompletionListener make it true.
      */
     private boolean initialStage = true;
     private boolean playing, playingStream;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+        initNavigation();
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        // Toolbar :it is a generalization of action bars for use within
+        // application layouts.
+        //Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        // DrawerLayout : it acts as a top-level container for window content
+        // that allows for interactive "drawer" views to be
+        // pulled out from the edge of the window.
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        //mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        // ActionBarDrawerToggle : This class provides a handy way to tie
+        // together the functionality of DrawerLayout and
+        // the framework ActionBar to implement the recommended design for
+        // navigation drawers.
+        final ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        //Set the ActionBarDrawerToggle in the layout
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        //Hide the default Actionbar
+        //getSupportActionBar().hide();
+        // Call syncState() from your Activity's onPostCreate to synchronize the
+        // indicator
+        // with the state of the linked DrawerLayout after
+        // onRestoreInstanceState has occurred
+        mDrawerToggle.syncState();
+
+        navList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                //navHeader.setText(categoryItems[position].toString());
+                mDrawerLayout.closeDrawers();
+            }
+        });
+
         ap = new AsyncPlayer("MyTest");
         mediaPlayer = null;
         playingStream = false;
         playing = false;
         playBt = (Button) findViewById(R.id.playBt);
-        Log.d("onCreate", "\n***************\n* VYTVORIL JSEM APPKU\n***************");
+       // Log.d("onCreate", "\n***************\n* VYTVORIL JSEM APPKU\n***************");
     }
 
     @Override
@@ -64,14 +133,23 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Toast.makeText(this, "Click on " + item.getItemId(), Toast.LENGTH_LONG).show();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings :
+                return true;
+            case R.id.home :
+                Toast.makeText(this, "Home click", Toast.LENGTH_LONG).show();
+                View navigation = findViewById(R.id.left_drawer);
+                if(mDrawerLayout.isDrawerOpen(navigation)){
+                    mDrawerLayout.closeDrawer(navigation);
+                } else {
+                    mDrawerLayout.openDrawer(navigation);
+                }
+                return true;
+
+            default : return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -84,7 +162,7 @@ public class MainActivity extends ActionBarActivity {
             mediaPlayer = null;
         }*/
     }
-
+    
     public void refresh(View v) {
         /*TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(getData1("http://evropa2.cz/mp3-archiv/"));
@@ -149,7 +227,23 @@ public class MainActivity extends ActionBarActivity {
         initialStage = true;
         playBt.setText(R.string.play);
     }
+    
+    private void initNavigation() {
+        // TODO Auto-generated method stub
+        //navHeader = (TextView) findViewById(R.id.navHeader);
+        // Find the ListView resource.
+        navList = (ListView) findViewById(R.id.left_drawer);
 
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll(Arrays.asList(categoryItems));
+
+        // Create ArrayAdapter using the planet navList.
+        navListAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, planetList);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        navList.setAdapter(navListAdapter);
+    }
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm.getActiveNetworkInfo();
@@ -161,7 +255,7 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public boolean isInternetAvailable() {
+    private boolean isInternetAvailable() {
         try {
             InetAddress ipAddr = InetAddress.getByName("google.com"); //You can replace it with your name
 
